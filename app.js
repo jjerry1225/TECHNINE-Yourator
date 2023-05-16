864468a46456f37e4bf54c9d77b01908875c4701
jjerry1225 <i183801225@gmail.com>
2023-05-16 16:52:05 +0800

    feat: add POST method /sortnum

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express()
 
 const routes = require('./routes')
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/hello', (req, res) => {
   res.json('Hello World')
 })
 
+router.post('/sortnum', (req, res) => {
+  const { sortnum } = req.body
+  
+  // 除了檢視是否是array格式外，也排除空陣列的情形，因若為空陣列則不需多進行排序的動作。
+  if(!Array.isArray(sortnum) || sortnum.length === 0) {
+    return res.status(400).json({ error: 'Must need to provide an Array with numbers.' })
+  }
+
+  sortnum.sort((a, b) => a - b)
+  return res.json({ sortnum })
+})
+
 module.exports = router
